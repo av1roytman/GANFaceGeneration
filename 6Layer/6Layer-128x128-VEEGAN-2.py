bf96162c0e36bcb1e309827db1f8914303ceae49
Chris Sigmund <csigmund@purdue.edu>
2024-01-29 21:38:18 -0500

    Moved all Generators & Discriminators to Basic file when appropriate

--- 6Layer/6Layer-128x128-VEEGAN-2.py
+++ 6Layer/6Layer-128x128-VEEGAN-2.py
@@ -9,6 +9,7 @@ import numpy as np
 import math
 from PIL import Image
 import os
+from Basic.Generator import Generator
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -67,42 +68,6 @@ for i in range(9):
 base = 'produced_images/6-layer'
 plt.savefig(os.path.join(base, 'celeba_sample_128.png'))
 plt.close(fig)
-
-# Define Generator Class
-class Generator(nn.Module):
-    def __init__(self):
-        super(Generator, self).__init__()
-        self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(100, 1024, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.ReLU(True),
-            # state size. 1024 x 4 x 4
-            nn.ConvTranspose2d(1024, 512, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(True),
-            # state size. 512 x 8 x 8
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(True),
-            # state size. 256 x 16 x 16
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(True),
-            # state size. 128 x 32 x 32
-            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False), 
-            nn.BatchNorm2d(64),
-            nn.ReLU(True),
-            # state size. 64 x 64 x 64
-
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),  # Output 3 channels for RGB
-            nn.Tanh()
-            # state size. 3 x 128 x 128
-        )
-
-    def forward(self, input):
-        return self.main(input)
-
 
 # Define the Discriminator
 class Discriminator(nn.Module):
