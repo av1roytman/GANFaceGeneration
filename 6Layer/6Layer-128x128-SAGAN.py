9db5340e7274f55c86ae43afa84beaaa2bd1870c
Avigdor Roytman <aroytman@purdue.edu>
2024-02-06 15:17:41 -0500

    added batch norm back to generator with spectral

--- 6Layer/6Layer-128x128-SAGAN.py
+++ 6Layer/6Layer-128x128-SAGAN.py
@@ -75,24 +75,24 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(100, 1024, 4, 1, 0, bias=False),
+            utils.spectral_norm(nn.ConvTranspose2d(100, 1024, 4, 1, 0, bias=False)),
             nn.BatchNorm2d(1024),
             nn.ReLU(True),
             # state size. 1024 x 4 x 4
 
-            nn.ConvTranspose2d(1024, 512, 4, 2, 1, bias=False),
+            utils.spectral_norm(nn.ConvTranspose2d(1024, 512, 4, 2, 1, bias=False)),
             nn.BatchNorm2d(512),
             nn.ReLU(True),
             # state size. 512 x 8 x 8
 
-            nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False),
+            utils.spectral_norm(nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False)),
             nn.BatchNorm2d(256),
             nn.ReLU(True),
             # state size. 256 x 16 x 16
 
             SelfAttention(256), # Self-Attention Layer
 
-            nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False),
+            utils.spectral_norm(nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)),
             nn.BatchNorm2d(128),
             nn.ReLU(True),
             # state size. 128 x 32 x 32
@@ -100,12 +100,12 @@ class Generator(nn.Module):
             SelfAttention(128), # Self-Attention Layer
             # state size. 128 x 32 x 32
 
-            nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False), 
+            utils.spectral_norm(nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False)), 
             nn.BatchNorm2d(64),
             nn.ReLU(True),
             # state size. 64 x 64 x 64
 
-            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),  # Output 3 channels for RGB
+            utils.spectral_norm(nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False)),  # Output 3 channels for RGB
             nn.Tanh()
             # state size. 3 x 128 x 128
         )
