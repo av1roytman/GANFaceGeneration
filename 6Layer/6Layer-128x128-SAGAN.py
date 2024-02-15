439a3f44c16a2e2d916950406623f0042dc71ed3
Avigdor Roytman <aroytman@purdue.edu>
2024-02-15 09:12:26 -0500

    Attempting Transformers from TransGAN and also refactoring in SAGAN to try and train better

--- 6Layer/6Layer-128x128-SAGAN.py
+++ 6Layer/6Layer-128x128-SAGAN.py
@@ -10,7 +10,122 @@ import math
 from PIL import Image
 import os
 
-device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
+def main():
+    device = torch.device("cuda:3" if torch.cuda.is_available() else "cpu")
+
+    # Reshape data and scale to [-1, 1]
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalizing to [-1, 1]
+    ])
+
+    image_dir = '../img_align_celeba'
+
+    # Create a dataset
+    dataset = CelebADataset(image_dir=image_dir, transform=transform)
+
+    # Batch Size Hyperparameter
+    global_batch_size = 64
+
+    # Create a data loader
+    # num_gpus = torch.cuda.device_count()
+    # print("Number of available GPUs:", num_gpus)
+    dataloader = DataLoader(dataset, batch_size=global_batch_size, shuffle=True, num_workers=8)
+
+    # Create a 3x3 grid for the images
+    fig, axes = plt.subplots(3, 3, figsize=(9, 9))
+
+    # Flatten the 2D array of axes for easy iteration
+    axes = axes.flatten()
+
+    for i in range(9):
+        image = dataset[i]
+        image = (image + 1) / 2.0 # Scale images to [0, 1] to visualize better
+        axes[i].imshow(np.transpose(image.numpy(), (1, 2, 0)))  # Directly use numpy and transpose here
+        axes[i].axis('off')  # Turn off axes for cleaner look
+
+    base = '../produced_images/SAGAN'
+    model_base = '../model_states/SAGAN'
+
+    plt.savefig(os.path.join(base, 'celeba_sample_128.png'))
+    plt.close(fig)
+
+    # Model Initialization
+    netG = Generator().to(device)
+    netD = Discriminator().to(device)
+
+    # Hyperparameters
+    num_epochs = 150
+    lr = 0.0001
+    beta1 = 0
+    beta2 = 0.9
+
+    # Binary cross entropy loss and optimizer
+    criterion = nn.BCELoss()
+
+    optimizerD = torch.optim.Adam(netD.parameters(), lr=lr*4, betas=(beta1, beta2))
+    optimizerG = torch.optim.Adam(netG.parameters(), lr=lr, betas=(beta1, beta2))
+
+    dataloader_length = len(dataloader)
+
+    gen_loss = []
+    dis_loss = []
+    batch_count = []
+
+    # Training Loop
+    for epoch in range(1, num_epochs + 1):
+        for i, data in enumerate(dataloader, 0):
+            # Transfer data tensor to GPU/CPU (device)
+            real_data = data.to(device)
+            batch_size = real_data.size(0)
+            label = torch.full((batch_size,), 1, dtype=torch.float, device=device)
+
+            # Train Discriminator
+            netD.zero_grad()
+            output = netD(real_data).view(-1)
+            errD_real = torch.mean(torch.relu(1.0 - output))
+            errD_real.backward()
+            
+            noise = torch.randn(batch_size, 100, 1, 1, device=device)
+            fake = netG(noise)
+            label.fill_(0)
+            output = netD(fake.detach()).view(-1)
+            errD_fake = torch.mean(torch.relu(1.0 + output))
+            errD_fake.backward()
+            errD = errD_real + errD_fake
+            optimizerD.step()
+
+            # Train Generator
+            netG.zero_grad()
+            label.fill_(1)  # The generator wants the discriminator to think the fake images are real
+            output = netD(fake).view(-1)
+            errG = -torch.mean(output)
+            errG.backward()
+            optimizerG.step()
+
+            # Save Losses for plotting later
+
+            if i % 50 == 0:
+                gen_loss.append(errG.item())
+                dis_loss.append(errD.item())
+                batch_count.append(i + dataloader_length * epoch)
+                print(f'[{epoch}/{num_epochs}][{i}/{dataloader_length}] Loss_D: {errD.item():.4f} Loss_G: {errG.item():.4f}')
+
+            if epoch % 25 == 0 and i == 0:
+                generate_images(netG, label=f'Epoch-{epoch}')
+                generate_loss_graphs(gen_loss, dis_loss, batch_count, base)
+                torch.save(netG.state_dict(), os.path.join(model_base, 'Gen-6Layer-128x128-SAGAN.pth'))
+                torch.save(netD.state_dict(), os.path.join(model_base, 'Dis-6Layer-128x128-SAGAN.pth'))
+
+    print("Training is complete!")
+
+    # Save the trained model
+    torch.save(netG.state_dict(), os.path.join(model_base, 'Gen-6Layer-128x128-SAGAN.pth'))
+
+    generate_images(netG, label='Final')
+    generate_loss_graphs(gen_loss, dis_loss, batch_count, base)
+
 
 class CelebADataset(Dataset):
     def __init__(self, image_dir, transform=None, num_samples=None):
@@ -31,42 +146,6 @@ class CelebADataset(Dataset):
             image = self.transform(image)
             
         return image
-
-# Reshape data and scale to [-1, 1]
-transform = transforms.Compose([
-    transforms.Resize((128, 128)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalizing to [-1, 1]
-])
-
-image_dir = '../img_align_celeba'
-
-# Create a dataset
-dataset = CelebADataset(image_dir=image_dir, transform=transform)
-
-# Batch Size Hyperparameter
-global_batch_size = 16
-
-# Create a data loader
-# num_gpus = torch.cuda.device_count()
-# print("Number of available GPUs:", num_gpus)
-dataloader = DataLoader(dataset, batch_size=global_batch_size, shuffle=True, num_workers=8)
-
-# Create a 3x3 grid for the images
-fig, axes = plt.subplots(3, 3, figsize=(9, 9))
-
-# Flatten the 2D array of axes for easy iteration
-axes = axes.flatten()
-
-for i in range(9):
-    image = dataset[i]
-    image = (image + 1) / 2.0 # Scale images to [0, 1] to visualize better
-    axes[i].imshow(np.transpose(image.numpy(), (1, 2, 0)))  # Directly use numpy and transpose here
-    axes[i].axis('off')  # Turn off axes for cleaner look
-
-base = '../produced_images/6-layer'
-plt.savefig(os.path.join(base, 'celeba_sample_128.png'))
-plt.close(fig)
 
 
 # Define Generator Class
@@ -90,7 +169,7 @@ class Generator(nn.Module):
             nn.ReLU(True),
             # state size. 256 x 16 x 16
 
-            SelfAttention(256), # Self-Attention Layer
+            # SelfAttention(256), # Self-Attention Layer
 
             utils.spectral_norm(nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)),
             nn.BatchNorm2d(128),
@@ -105,7 +184,7 @@ class Generator(nn.Module):
             nn.ReLU(True),
             # state size. 64 x 64 x 64
 
-            SelfAttention(64), # Self-Attention Layer
+            # SelfAttention(64), # Self-Attention Layer
 
             utils.spectral_norm(nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False)),  # Output 3 channels for RGB
             nn.Tanh()
@@ -124,14 +203,14 @@ class Discriminator(nn.Module):
             # Input: 3 x 128 x 128
             utils.spectral_norm(nn.Conv2d(3, 64, 3, stride=2, padding=1)),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout(0.5), # Dropout Layer
+            nn.Dropout(0.1), # Dropout Layer
             # state size. 64 x 64 x 64
 
-            SelfAttention(64), # Self-Attention Layer
+            # SelfAttention(64), # Self-Attention Layer
 
             utils.spectral_norm(nn.Conv2d(64, 128, 3, stride=2, padding=1)),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout(0.5), # Dropout Layer
+            nn.Dropout(0.1), # Dropout Layer
             # state size. 128 x 32 x 32
 
             SelfAttention(128), # Self-Attention Layer
@@ -139,19 +218,19 @@ class Discriminator(nn.Module):
 
             utils.spectral_norm(nn.Conv2d(128, 256, 3, stride=2, padding=1)),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout(0.5), # Dropout Layer
+            nn.Dropout(0.1), # Dropout Layer
             # state size. 256 x 16 x 16
 
-            SelfAttention(256), # Self-Attention Layer
+            # SelfAttention(256), # Self-Attention Layer
 
             utils.spectral_norm(nn.Conv2d(256, 512, 3, stride=2, padding=1)),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout(0.5), # Dropout Layer
+            nn.Dropout(0.1), # Dropout Layer
             # state size. 512 x 8 x 8
 
             utils.spectral_norm(nn.Conv2d(512, 1024, 3, stride=2, padding=1)),
             nn.LeakyReLU(0.2, inplace=True),
-            # nn.Dropout(0.5), # Dropout Layer
+            nn.Dropout(0.1), # Dropout Layer
             # state size. 1024 x 4 x 4
 
             utils.spectral_norm(nn.Conv2d(1024, 1, kernel_size=4, stride=1, padding=0)),
@@ -190,101 +269,42 @@ class SelfAttention(nn.Module):
 
         return self.gamma * out + x  # Skip connection
     
-# Model Initialization
-netG = Generator().to(device)
-netD = Discriminator().to(device)
 
-# Hyperparameters
-num_epochs = 30
-lr = 0.0001
-beta1 = 0
-beta2 = 0.9
+def generate_images(netG, base, fixed_noise, label=""):
+    # After training, use the generator to produce images from the fixed noise vectors
+    netG.eval()
+    with torch.no_grad():
+        fake_images = netG(fixed_noise).detach().cpu()
 
-# Binary cross entropy loss and optimizer
-criterion = nn.BCELoss()
+    fig, axes = plt.subplots(3, 3, figsize=(9, 9))
 
-optimizerD = torch.optim.Adam(netD.parameters(), lr=lr*4, betas=(beta1, beta2))
-optimizerG = torch.optim.Adam(netG.parameters(), lr=lr, betas=(beta1, beta2))
+    # Flatten the 2D array of axes for easy iteration
+    axes = axes.flatten()
 
-dataloader_length = len(dataloader)
+    for i in range(9):
+        image = fake_images[i]
+        image = (image + 1) / 2.0 # Scale images to [0, 1] to visualize better
+        axes[i].imshow(np.transpose(image.numpy(), (1, 2, 0)))  # Directly use numpy and transpose here
+        axes[i].axis('off')  # Turn off axes for cleaner look
 
-gen_loss = []
-dis_loss = []
-batch_count = []
+    plt.savefig(os.path.join(base, f'6Layer-128x128-SAGAN-{label}.png'))
+    plt.close(fig)
 
-# Training Loop
-for epoch in range(1, num_epochs + 1):
-    for i, data in enumerate(dataloader, 0):
-        # Transfer data tensor to GPU/CPU (device)
-        real_data = data.to(device)
-        batch_size = real_data.size(0)
-        label = torch.full((batch_size,), 1, dtype=torch.float, device=device)
+    # return netG to training mode
+    netG.train()
 
-        # Train Discriminator
-        netD.zero_grad()
-        output = netD(real_data).view(-1)
-        errD_real = torch.mean(torch.relu(1.0 - output))
-        errD_real.backward()
-        
-        noise = torch.randn(batch_size, 100, 1, 1, device=device)
-        fake = netG(noise)
-        label.fill_(0)
-        output = netD(fake.detach()).view(-1)
-        errD_fake = torch.mean(torch.relu(1.0 + output))
-        errD_fake.backward()
-        errD = errD_real + errD_fake
-        optimizerD.step()
+def generate_loss_graphs(gen_loss, dis_loss, batch_count, base):
+    # Graph the Loss
+    plt.figure(figsize=(10, 5))
+    plt.title("Generator and Discriminator Loss During Training")
+    plt.plot(batch_count, gen_loss, label="Generator")
+    plt.plot(batch_count, dis_loss, label="Discriminator")
+    plt.xlabel("Batch Count")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig(os.path.join(base, f'Loss_6Layer-128x128-SAGAN-{batch_count}.png'))
+    plt.close()
 
-        # Train Generator
-        netG.zero_grad()
-        label.fill_(1)  # The generator wants the discriminator to think the fake images are real
-        output = netD(fake).view(-1)
-        errG = -torch.mean(output)
-        errG.backward()
-        optimizerG.step()
 
-        # Save Losses for plotting later
-
-        if i % 50 == 0:
-            gen_loss.append(errG.item())
-            dis_loss.append(errD.item())
-            batch_count.append(i + dataloader_length * epoch)
-            print(f'[{epoch}/{num_epochs}][{i}/{dataloader_length}] Loss_D: {errD.item():.4f} Loss_G: {errG.item():.4f}')
-
-print("Training is complete!")
-
-# Save the trained model
-model_base = '../model_states/6-layer'
-torch.save(netG.state_dict(), os.path.join(model_base, 'Gen-6Layer-128x128-SAGAN.pth'))
-
-fixed_noise = torch.randn(global_batch_size, 100, 1, 1, device=device)
-
-# After training, use the generator to produce images from the fixed noise vectors
-netG.eval()
-with torch.no_grad():
-    fake_images = netG(fixed_noise).detach().cpu()
-
-fig, axes = plt.subplots(3, 3, figsize=(9, 9))
-
-# Flatten the 2D array of axes for easy iteration
-axes = axes.flatten()
-
-for i in range(9):
-    image = fake_images[i]
-    image = (image + 1) / 2.0 # Scale images to [0, 1] to visualize better
-    axes[i].imshow(np.transpose(image.numpy(), (1, 2, 0)))  # Directly use numpy and transpose here
-    axes[i].axis('off')  # Turn off axes for cleaner look
-
-plt.savefig(os.path.join(base, '6Layer-128x128-SAGAN.png'))
-plt.close(fig)
-
-# Graph the Loss
-plt.figure(figsize=(10, 5))
-plt.title("Generator and Discriminator Loss During Training")
-plt.plot(batch_count, gen_loss, label="Generator")
-plt.plot(batch_count, dis_loss, label="Discriminator")
-plt.xlabel("Batch Count")
-plt.ylabel("Loss")
-plt.legend()
-plt.savefig(os.path.join(base, 'Loss_6Layer-128x128-SAGAN.png'))
-plt.close()
+if __name__ == '__main__':
+    main()
