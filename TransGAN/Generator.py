07d8fd5d83681ff0b8d962d11ef20e6453ebfaf8
Avigdor Roytman <aroytman@purdue.edu>
2024-03-05 14:44:57 -0500

    Positional Encoding fixes again

--- TransGAN/Generator.py
+++ TransGAN/Generator.py
@@ -19,40 +19,39 @@ class Generator(nn.Module):
             nn.ReLU(True)
         )
 
-
         # Stage 1: Transformer blocks
-        self.pos_enc_stage1 = PositionalEncoding(self.initial_dimension * self.initial_dimension, embed_dim)
+        self.pos_enc_stage1 = PositionalEncoding(embed_dim, self.initial_dimension * self.initial_dimension)
         self.blocks_stage1 = nn.Sequential(*[TransformerBlock(embed_dim, ff_dim, 8, 8, dropout) for _ in range(5)])
 
         # Stage 2: Upsampling and transformer blocks
         self.upsample_stage2 = UpsamplingBlock(embed_dim, 8, 8)
-        self.pos_enc_stage2 = PositionalEncoding(16 * 16, embed_dim)
+        self.pos_enc_stage2 = PositionalEncoding(embed_dim, 16 * 16)
         self.blocks_stage2 = nn.Sequential(*[TransformerBlock(embed_dim, ff_dim, 16, 16, dropout) for _ in range(4)])
 
         # Stage 3: Pixel shuffle and transformer blocks
         self.pixel_shuffle_stage3 = UpsampleBlock_PixelShuffle(embed_dim, 16, 16)
-        self.pos_enc_stage3 = PositionalEncoding(32 * 32, embed_dim)
+        self.pos_enc_stage3 = PositionalEncoding(embed_dim // 4, 32 * 32)
         self.blocks_stage3 = nn.Sequential(*[GridTransformerBlock(embed_dim // 4, ff_dim, 32, 32, dropout) for _ in range(4)])
 
         # Stage 4: Pixel shuffle and grid transformer blocks
         self.pixel_shuffle_stage4 = UpsampleBlock_PixelShuffle(embed_dim // 4, 32, 32)
-        self.pos_enc_stage4 = PositionalEncoding(64 * 64, embed_dim)
+        self.pos_enc_stage4 = PositionalEncoding(embed_dim // 16, 64 * 64)
         self.blocks_stage4 = nn.Sequential(*[GridTransformerBlock(embed_dim // 16, ff_dim, 64, 64, dropout) for _ in range(4)])
 
         # Stage 5: Pixel shuffle and grid transformer blocks
         self.pixel_shuffle_stage5 = UpsampleBlock_PixelShuffle(embed_dim // 16, 64, 64)
-        self.pos_enc_stage5 = PositionalEncoding(128 * 128, embed_dim)
+        self.pos_enc_stage5 = PositionalEncoding(embed_dim // 64, 128 * 128)
         self.blocks_stage5 = nn.Sequential(*[GridTransformerBlock(embed_dim // 64, ff_dim, 128, 128, dropout) for _ in range(4)])
 
         # Final linear layer to map to RGB image
 
-        self.reshape = nn.Unflatten(1, (128, 128))
         self.to_rgb = nn.Conv2d(embed_dim // 64, 3, kernel_size=1)
 
     def forward(self, z):
         z = z.view(z.shape[0], -1) # size: (batch_size, noise_dim)
         x = self.mlp(z) # size: (batch_size, 8 * 8 * embed_dim)
         x = x.view(z.shape[0], 64, self.embed_dim) # size: (batch_size, 64, embed_dim)
+        print(f'x.shape: {x.shape}')
 
         # Stage 1
         x = self.pos_enc_stage1(x) # size: (batch_size, 64, embed_dim)
--- TransGAN/PositionalEncoding.py
+++ TransGAN/PositionalEncoding.py
@@ -11,7 +11,7 @@ class PositionalEncoding(nn.Module):
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe = pe.unsqueeze(0)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
